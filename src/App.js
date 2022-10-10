44430146c2cff7d7061d53c72f9aeef8df021aa6
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-10 15:32:24 +0700

    refactor: component app from react component tu functonal component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import Content from "./component/content/Content";
 import FormCard from "./component/form/FormCard";
 
-class App extends React.Component {
-    render() {
-        return <div className="container-app">
-            <FormCard />
-            <Content />
-        </div>
-    }
-}
+function App() {
+    return <div className="container-app">
+        <FormCard />
+        <Content />
+    </div>
+};
 
 export default App;
